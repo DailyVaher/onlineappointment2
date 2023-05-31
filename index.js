a4d796c57bc9c585f53ef1d9d8149e33a6f6fa69
DailyVaher <daily.vaher@vikk.ee>
2023-05-31 08:44:53 +0300

    As a user I can add and see my appointments [Delivers #184819174]

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcrypt')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000
-const {v4: uuidv4} = require('uuid')
+let expressWs = require('express-ws')(app);
+const {v4: uuidv4} = require('uuid');
 
 // Add Swagger UI
 const swaggerUi = require('swagger-ui-express');
@@ -14,17 +15,45 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.static('public'))
 app.use(express.json())
+app.ws('/', function (ws, req) {
+    ws.on('message', function (msg) {
+        expressWs.getWss().clients.forEach(client => client.send(msg));
+    });
+    console.log('socket', req.testing);
+});
 
 const users = [
     {id: 1, email: 'admin', password: '$2b$10$0EfA6fMFRDVQWzU0WR1dmelPA7.qSp7ZYJAgneGsy2ikQltX2Duey'} // KollneKollne
 ]
+
+const appointments = [
+    {
+        id: 1,
+        title: 'Appointment 1',
+        content: 'This is the content of appointment 1',
+        userId: 1
+    },
+    {
+        id: 2,
+        title: 'Appointment 2',
+        content: 'This is the content of appointment 2',
+        userId: 2
+    },
+    {
+        id: 3,
+        title: 'Appointment 3',
+        content: 'This is the content of appointment 3',
+        userId: 1
+    }
+]
+
 let sessions = [
-    // {id: '123', userId: 1}
+    {id: '123', userId: 1}
 ]
 
 function tryToParseJson(jsonString) {
     try {
-        const o = JSON.parse(jsonString);
+        let o = JSON.parse(jsonString);
         if (o && typeof o === "object") {
             return o;
         }
@@ -33,6 +62,7 @@ function tryToParseJson(jsonString) {
     return false;
 }
 
+const delay = (ms) => new Promise(res => setTimeout(res, ms));
 app.post('/users', async (req, res) => {
 
     // Validate email and password
@@ -40,28 +70,32 @@ app.post('/users', async (req, res) => {
     if (req.body.password.length < 8) return res.status(400).send('Password must be at least 8 characters long')
     if (!req.body.email.match(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) return res.status(400).send('Email must be in a valid format')
 
-    //Check if email already exists
+    // Check if email already exists
     if (users.find(user => user.email === req.body.email)) return res.status(409).send('Email already exists')
+
+    // Find user in database
+    //const user = users.find(user => user.email === req.body.email)
+    //if (!user) return res.status(404).send('User not found')
 
     // Try to contact the mail server and send a test email without actually sending it
     try {
         const result = await verifyEmail(req.body.email);
         if (!result.success) {
-            return res.status(400).send('Email is invalid: ' + result.info)
+            return res.status(400).send('Invalid email: ' + result.info)
         }
         console.log('Email verified')
     } catch (error) {
         const errorObject = tryToParseJson(error)
         if (errorObject && errorObject.info) {
-            return res.status(400).send('Email is invalid: ' + errorObject.info)
+            return res.status(400).send('Invalid email: ' + errorObject.info)
         }
-        return res.status(400).send('Email is invalid: ' + error)
+        return res.status(400).send('Invalid email: ' + error)
     }
 
     // Hash password
     let hashedPassword
     try {
-        hashedPassword = await bcrypt.hash(req.body.password, 10);
+        hashedPassword = await bcrypt.hash(req.body.password,10);
     } catch (error) {
         console.error(error);
     }
@@ -73,12 +107,10 @@ app.post('/users', async (req, res) => {
     users.push({id: maxId + 1, email: req.body.email, password: hashedPassword})
 
     res.status(201).end()
-
 })
 
 // POST /sessions
 app.post('/sessions', async (req, res) => {
-
     // Validate email and password
     if (!req.body.email || !req.body.password) return res.status(400).send('Email and password are required')
 
@@ -86,14 +118,14 @@ app.post('/sessions', async (req, res) => {
     const user = users.find(user => user.email === req.body.email)
     if (!user) return res.status(404).send('User not found')
 
-    // Compare password
+    // Compare passwords
     try {
         if (await bcrypt.compare(req.body.password, user.password)) {
-
+            // Passwords match
             // Create session
             const session = {id: uuidv4(), userId: user.id}
 
-            // Add session to sessions array
+            //Add session to sessions array
             sessions.push(session)
 
             // Send session to client
@@ -106,27 +138,26 @@ app.post('/sessions', async (req, res) => {
         console.error(error);
         res.status(500).send('Internal server error')
     }
-
 })
 
 function authorizeRequest(req, res, next) {
-    // Check that there is an Authorization header
-    if (!req.headers.authorization) return res.status(401).send('Authorization header is missing')
+    // Check that there is an authorization header
+    if (!req.headers.authorization) return res.status(401).send('Missing authorization header')
 
-    // Check that the Authorization header is in the correct format
+    // Check that the authorization header is in the correct format
     const authorizationHeader = req.headers.authorization.split(' ')
-    if (authorizationHeader.length !== 2 || authorizationHeader[0] !== 'Bearer') return res.status(401).send('Invalid Authorization header')
+    if (authorizationHeader.length !== 2 || authorizationHeader[0] !== 'Bearer') return res.status(400).send('Invalid authorization header')
 
-    // Get the session id from the Authorization header
+    // Get sessionId from authorization header
     const sessionId = authorizationHeader[1]
 
-    // Find the session in the sessions array
+    // Find session in sessions array
     const session = sessions.find(session => session.id === sessionId)
-    if (!session) return res.status(401).send('Invalid session id')
+    if (!session) return res.status(401).send('Invalid session')
 
     // Check that the user exists
     const user = users.find(user => user.id === session.userId)
-    if (!user) return res.status(401).send('Invalid user id')
+    if (!user) return res.status(401).send('Invalid session')
 
     // Add user to request object
     req.user = user
@@ -139,15 +170,72 @@ function authorizeRequest(req, res, next) {
 
 }
 
-app.delete('/sessions', authorizeRequest, (req, res) => {
+app.get('/appointments', authorizeRequest, async (req, res) => {
+    await delay(1000)
+    // Get appointments for user
+    const appointmentsForUser = appointments.filter(appointment => appointment.userId === req.user.id)
 
+    // Send appointments to client
+    res.send(appointmentsForUser)
+})
+
+app.post('/appointments', authorizeRequest, (req, res) => {
+
+    // Validate title and content
+    if (!req.body.title || !req.body.content) return res.status(400).send('Title and content are required')
+
+    // Find max id
+    const maxId = appointments.reduce((max, appointment) => appointment.id > max ? appointment.id : max, appointments[0].id)
+
+    // Save note to database
+    appointments.push({id: maxId + 1, title: req.body.title, content: req.body.content, userId: req.user.id})
+
+    // Send note to client
+    res.status(201).send(appointments[appointments.length - 1])
+
+})
+app.delete('/appointments/:id', authorizeRequest, (req, res) => {
+
+    // Find appointment in database
+    const appointment = appointments.find(appointment => appointment.id === parseInt(req.params.id))
+    if (!appointment) return res.status(404).send('Appointment not found')
+
+    // Check that the appointment belongs to the user
+    if (appointment.userId !== req.user.id) return res.status(401).send('Unauthorized')
+
+    // Remove appointment from active database
+    appointment.splice(appointment.indexOf(appointment), 1)
+    expressWs.getWss().clients.forEach(client => client.send(appointment.id));
+    res.status(204).end()
+})
+
+app.put('/appointments/:id', authorizeRequest, (req, res) => {
+
+    // Find appointment in database
+    const appointment = appointments.find(appointment => appointment.id === parseInt(req.params.id))
+    if (!appointment) return res.status(404).send('Appointment not found')
+
+    // Check that the appointment belongs to the user
+    if (appointment.userId !== req.user.id) return res.status(401).send('Unauthorized')
+
+    // Validate title and content
+    if (!req.body.title || !req.body.content) return res.status(400).send('Title and content are required')
+
+    // Update appointment
+    appointment.title = req.body.title
+    appointment.content = req.body.content
+    expressWs.getWss().clients.forEach(client => client.send(appointment.id));
+
+    // Send appointment to client
+    res.send(appointment)
+})
+
+app.delete('/sessions', authorizeRequest, (req, res) => {
     // Remove session from sessions array
     sessions = sessions.filter(session => session.id !== req.session.id)
 
     res.status(204).end()
-
 })
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
@@ -157,7 +245,7 @@ function verifyEmail(email) {
         verifier.verify(email, (err, info) => {
             console.log(err, info);
             if (err) {
-                reject(JSON.stringify([err, info]));
+                reject(JSON.stringify(info));
             } else {
                 resolve(info);
             }
